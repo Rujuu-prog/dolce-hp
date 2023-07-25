5a93db1381d635a38ee25d75e547ee1fa4f7f47d
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2023-07-25 23:45:04 +0900

    :bug: add styled-components config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     optimizeFonts: true,
   },
+  compiler: {
+    styledComponents: {
+      displayName: process.env.NODE_ENV !== "production",
+      ssr: true,
+      fileName: true,
+      // 以下は今後サポートされる予定のため、一旦false
+      minify: false,
+      transpileTemplateLiterals: false,
+      pure: false,
+    },
+  },
 };
 
 module.exports = nextConfig;
